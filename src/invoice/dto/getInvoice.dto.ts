70bd48fb9ca0fc330dcd652a448a03a53f0a8abc
Anastasia Karachova <57650422+undefined-person@users.noreply.github.com>
2023-02-02 12:50:59 +0200

    fix: remove unused library

--- src/invoice/dto/getInvoice.dto.ts
+++ src/invoice/dto/getInvoice.dto.ts
@@ -1,17 +1,13 @@
-import { ApiProperty } from '@nestjs/swagger'
 import { IsNumber, IsOptional, IsString } from 'class-validator'
 
 export class GetInvoiceDto {
-  @ApiProperty({ type: Array<string>, description: 'Status' })
   @IsString({ each: true })
   @IsOptional()
   readonly status?: Array<string>
 
-  @ApiProperty({ type: Number, description: 'Limit' })
   @IsNumber()
   readonly limit?: number
 
-  @ApiProperty({ type: Number, description: 'Offset' })
   @IsNumber()
   readonly offset?: number
 }
